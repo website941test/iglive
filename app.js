3ef8f34059f0e13e93e975c92a5b0200a3c4854a
website941test <126428176+website941test@users.noreply.github.com>
2023-02-26 19:04:08 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+var track = null;
+
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");
+
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+        });
+}
+
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
